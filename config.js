f31f1399b33819d5caa159082dc9a5ab7730a6ac
Drake3robin <140759348+Drake3robin@users.noreply.github.com>
2025-05-31 08:08:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
     
     // ===== BOT CORE SETTINGS =====
-    SESSION_ID: process.env.SESSION_ID || "",
+    SESSION_ID: process.env.SESSION_ID || "SUBZERO-MD~YLbtpbwODGQgbc8zWIM1",
     PREFIX: process.env.PREFIX || ".",
     BOT_NAME: process.env.BOT_NAME || "SUBZERO-MD",
     MODE: process.env.MODE || "public",
